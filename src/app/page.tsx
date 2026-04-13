1b7ce27c967b5b5e2cd3b04478969c8f9446049e
marcojr73 <marcojuniorengineer@gmail.com>
2026-04-13 16:53:07 -0300

    move rotas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import App from "../app";
-import { TranslateProvider } from "../providers/translateProvider";
-import "../translate/i18n";
+import App from "./app";
+import { TranslateProvider } from "./providers/translateProvider";
+import "./translate/i18n";
 
 export default function Home() {
     return (
